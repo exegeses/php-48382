779b54cae5e981733de2dfcd3ab81544d8c39b67
Marcos <exegeses@gmail.com>
2022-03-12 10:30:27 -0300

    material clase 4

---
+++ clase4/locaciones.php
@@ -0,0 +1,63 @@
+<?php
+    $fotos =
+        [
+            'angkor', 'azul', 'basil', 'burj',
+            'colosseo', 'easter', 'eiffel',
+            'gizah', 'ha-long', 'liberty',
+            'machu', 'opera', 'palace', 'petra',
+            'sagrada', 'santorini', 'taj',
+            'wall'
+        ];
+    $paises =
+        [
+            'Cambodia', 'Turquía', 'Rusia',
+            'Dubai', 'Italia', 'Chile',
+            'Francia', 'Egipto', 'Vietnam',
+            'USA', 'Peru', 'Australia',
+            'Tailandia', 'Jordania', 'España',
+            'Grecia', 'India', 'China',
+        ];
+    $descripciones =
+        [
+            'Angkor Wat, Angkor',
+            'Mezquita azul, Estambul',
+            'Catedral de San Basilio, Moscu',
+            'Burj Khalifa, Dubai',
+            'El Coliseo, Roma', 'Isla de Pascua, Chile',
+            'Tour Eiffel, París',
+            'Gran Pirámide de Guiza, Guiza',
+            'Hạ Long Bay, Quang Ninh, Vietnam',
+            'Estatua de la Libertad, New York',
+            'Machu Picchu, Perú',
+            'Opera House, Sydney', 'Grand Palace, Bangkok', 'petra',
+            'La Sagrada Familia, Barcelona',
+            'Santorini, Archipiélago de las Cícladas ',
+            'Taj Mahal, Agra',
+            'La Gran Muralla, Jinshanling'
+        ];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<main>
+
+        <article>
+            <img src="locaciones/eiffel.jpg">
+            <h2>país</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto assumenda atque corporis est expedita hic ipsam laborum maxime quas.
+            </p>
+        </article>
+
+
+</main>
+
+</body>
+</html>
