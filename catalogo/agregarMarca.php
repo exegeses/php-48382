dfd0282c7e69753fdaca5e79ca71c461dcb432ef
Marcos <exegeses@gmail.com>
2022-03-26 11:43:02 -0300

    aplicación de altra de una marca

--- catalogo/agregarMarca.php
+++ catalogo/agregarMarca.php
@@ -1,24 +1,28 @@
 <?php
+    require 'funciones/conexion.php';
+    require 'funciones/marcas.php';
+    $chequeo = agregarMarca();
     include 'layout/header.php';
     include 'layout/nav.php';
+    
+    $css = 'danger';
+    $mensaje = 'No se pudo agregar la marca.';
+    if ( $chequeo ){
+        $css = 'success';
+        $mensaje = 'Marca agregada correctamente.';
+    }
 ?>
 
     <main class="container py-4">
         <h1>Alta de una marca</h1>
 
-        <div class="alert alert-success col-7 mx-auto">
-            Marca agregada correctamente.
+        <div class="alert alert-<?= $css ?> col-7 mx-auto">
+            <?= $mensaje ?>
             <a href="adminMarcas.php" class="btn btn-light m-2">
                 Volver a panel de marcas
             </a>
         </div>
 
-        <div class="alert alert-danger col-7 mx-auto">
-            No se pudo agregar la marca.
-            <a href="adminMarcas.php" class="btn btn-light m-2">
-                Volver a panel de marcas
-            </a>
-        </div>
 
     </main>
 
