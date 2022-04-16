cced7e8b6aab513e9af1f1d8a9f7363e8e138230
Marcos <exegeses@gmail.com>
2022-04-16 11:58:25 -0300

    portada

--- catalogo/index.php
+++ catalogo/index.php
@@ -1,4 +1,7 @@
 <?php
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $productos = listarProductos();
     include 'layout/header.php';
     include 'layout/nav.php';
 ?>
@@ -7,71 +10,23 @@
         <h1>Catálogo de productos</h1>
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4 py-5">
 
+<?php
+        while ( $producto = mysqli_fetch_assoc( $productos ) ) {
+?>
             <article class="col d-flex align-items-start">
                 <div class="m-1 producto">
-                    <img src="productos/muestra.png">
-                    <h2>Nombre del producto</h2>
-                    <span class="precio">$1000</span>
+                    <img src="productos/<?= $producto['prdImagen'] ?>">
+                    <h2><?= $producto['prdNombre'] ?></h2>
+                    <span class="precio">$<?= $producto['prdPrecio'] ?></span>
                     <p>
-                        Marca: marca <br>
-                        Categoría: categoría <br>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, odio!
+                        Marca: <?= $producto['mkNombre'] ?> <br>
+                        Categoría: <?= $producto['catNombre'] ?> <br>
                     </p>
                 </div>
             </article>
-
-            <article class="col d-flex align-items-start">
-                <div class="m-1 producto">
-                    <img src="productos/muestra.png">
-                    <h2>Nombre del producto</h2>
-                    <span class="precio">$1000</span>
-                    <p>
-                        Marca: marca <br>
-                        Categoría: categoría <br>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, odio!
-                    </p>
-                </div>
-            </article>
-
-            <article class="col d-flex align-items-start">
-                <div class="m-1 producto">
-                    <img src="productos/muestra.png">
-                    <h2>Nombre del producto</h2>
-                    <span class="precio">$1000</span>
-                    <p>
-                        Marca: marca <br>
-                        Categoría: categoría <br>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, odio!
-                    </p>
-                </div>
-            </article>
-
-            <article class="col d-flex align-items-start">
-                <div class="m-1 producto">
-                    <img src="productos/muestra.png">
-                    <h2>Nombre del producto</h2>
-                    <span class="precio">$1000</span>
-                    <p>
-                        Marca: marca <br>
-                        Categoría: categoría <br>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, odio!
-                    </p>
-                </div>
-            </article>
-
-            <article class="col d-flex align-items-start">
-                <div class="m-1 producto">
-                    <img src="productos/muestra.png">
-                    <h2>Nombre del producto</h2>
-                    <span class="precio">$1000</span>
-                    <p>
-                        Marca: marca <br>
-                        Categoría: categoría <br>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, odio!
-                    </p>
-                </div>
-            </article>
-
+<?php
+        }
+?>
 
         </div>
         
